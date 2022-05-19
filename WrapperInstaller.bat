4bf62992a07cc10587a02fa26e7063c69d432484
Kia <82962111+KiaWeb@users.noreply.github.com>
2022-05-19 16:01:19 -0500

    Update WrapperInstaller.bat

--- WrapperInstaller.bat
+++ WrapperInstaller.bat
@@ -1,5 +1,6 @@
 @echo off
 winget install git
+winget install -e --id OpenJS.NodeJS -v 14.14.0
 git clone https://github.com/Wrapper-Offline/Wrapper-Offline.git ./LVM
 echo Installed!
 echo Press the ENTER key.
