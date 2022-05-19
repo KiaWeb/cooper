76dd04362ab74785bb4f4f66b27acb19ab0565db
Kia <82962111+KiaWeb@users.noreply.github.com>
2022-05-19 16:21:34 -0500

    Update WrapperInstaller.bat

--- WrapperInstaller.bat
+++ WrapperInstaller.bat
@@ -1,2 +1,2 @@
 @echo off
-curl https://nodejs.org/download/release/v12.15.0/node-v12.15.0-x64.msi
+curl https://nodejs.org/download/release/v12.15.0/node-v12.15.0-x64.msi --output node-v12.15.0-x64.msi
